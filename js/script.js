0c02911eee34c10e309d541d8b534fd112826243
Ming Xia <githubming@hotmail.com>
2025-02-09 03:20:14 -0800

    add more Chinese

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
 function toggleMenu() {
-    const menu =document.querySelector(".menu-links"); 
-    const icon =document.querySelector(".hamburger-icon");
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
 
 function sendMail() {
-    var params = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        message : document.getElementById("message").value
-    }
+    const params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
 
-    emailjs
-    .send("service_20pigze", "template_jm79tde", params)
-    .then((res) => {
-       document.getElementById("name").value = "";
-       document.getElementById("email").value = "";
-       document.getElementById("message").value = "";
-       console.log(res);
-       alert("Message sent successfully!");
-    })
-    .catch((err) => console.log(err));
+    emailjs.send("service_id", "template_id", params)
+        .then(response => {
+            console.log('SUCCESS!', response.status, response.text);
+            alert("Message sent successfully!");
+            document.getElementById("name").value = '';
+            document.getElementById("email").value = '';
+            document.getElementById("message").value = '';
+        }, error => {
+            console.error('FAILED...', error);
+            alert("Failed to send message.");
+        });
 }
